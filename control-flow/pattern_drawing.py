c04c998cabdc491b6945c23d01bc9122b711e9ae
Fuad Jemal <fuad47722@gmail.com>
2025-05-29 21:43:44 +0300

    Refactor pattern drawing logic for improved readability

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -3,7 +3,9 @@ pattern = int(input("Enter the size of the pattern: "))
 if pattern > 0:
     i = 1
     while i <= pattern:
-        print("*" * i)
+        for j in range(i):
+            print("*", end="")
+        print()
         i += 1
 else:
     print("Please enter a positive number.")
