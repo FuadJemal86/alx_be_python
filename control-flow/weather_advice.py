0f2985eb8fa3839b89d615121d35a3db9558adf8
Fuad Jemal <fuad47722@gmail.com>
2025-05-29 15:20:34 +0300

    Refactor weather advice prompts for consistency and clarity

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,15 +1,12 @@
+# weather_advice.py
 
-weather = str(input('What is the weather like today? (sunny/rainy/cold):'))
+weather = str(input("What's the weather like today? (sunny/rainy/cold): "))
 
 if weather == 'sunny':
-   print(' Wear a t-shirt and sunglasses.')
-
+    print('Wear a t-shirt and sunglasses.')
 elif weather == 'rainy':
-   print(' Wear a t-shirt and sunglasses.')
+    print("Don't forget your umbrella and a raincoat.")
 elif weather == 'cold':
-   print('Make sure to wear a warm coat and a scarf.')
+    print('Make sure to wear a warm coat and a scarf.')
 else:
-   print("Sorry, I don't have recommendations for this weather.")
-    
-    
-
+    print("Sorry, I don't have recommendations for this weather.")
