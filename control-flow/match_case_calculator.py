5d933e8d71c94e2d69974f2a4572c0a79b2a9b97
Fuad Jemal <fuad47722@gmail.com>
2025-05-29 16:48:51 +0300

    Refactor match_case_calculator for improved clarity and consistency

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,23 +1,22 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
+operation = input("Choose the operation (+, -, *, /): ")
 
-num1 = float (input("Enter the first number: "))
-num2 = float (input("Enter the second number:"))
-
-operation = input(' Choose the operation (+, -, *, /):')
-
-if operation == "+": 
-    addition = float(num1 + num2)
-    print(f'The result is {addition}')
+if operation == "+":
+    result = num1 + num2
+    print(f'The result is {result}')
 elif operation == "-":
-    subtract = float(num1 - num2)
-    print(f'The result is {subtract}')
+    result = num1 - num2
+    print(f'The result is {result}')
 elif operation == "*":
-    multiply = float(num1 * num2)
-    print(f'The result is {multiply}')
+    result = num1 * num2
+    print(f'The result is {result}')
 elif operation == "/":
     if num2 != 0:
-        divide = float(num1/num2)
+        result = num1 / num2
+        print(f'The result is {result}')
     else:
         print('Cannot divide by zero.')
-
-    
+else:
+    print("Invalid operation.")
